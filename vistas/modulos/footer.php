b54b1e64f42deb3159504ce8aba45a5f46cf2234
Alexander SB <alexsb12348@gmail.com>
2018-12-26 09:32:57 -0500

    Agregado Menus
    Menu Lateral, Icono de Usuario, Logos, Header, Footer, etc

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2018 <a href="https://www.google.com" target="_blank">
+		Alexander Salluca Bautista
+	</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
